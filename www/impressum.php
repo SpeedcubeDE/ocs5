eb58afdb5015601406aec145e05971780e37fe56
Felk <studio.felk@googlemail.com>
2016-06-13 22:21:23 +0200

    removed obsolete paragraph about being hosted on picocom.net

--- www/impressum.php
+++ www/impressum.php
@@ -5,7 +5,6 @@ require_once 'includes/header.php';
 <h2>Impressum</h2>
 <p>Das OCS (Online-Cubing-System) ist eine nicht-kommerzielle Webanwendung, welche das gemeinsame <a href="http://de.wikipedia.org/wiki/Speedcubing" target="_blank">Speedcuben</a> und Chatten mit anderen ermöglicht.</p>
 <p>Es wurde entwickelt von Justin Tappeser und Felix König. Genauere Informationen auf '<a href="about.php">Über das OCS</a>'. Alle Rechte vorbehalten.</p>
-<p>Gehostet wird es netterweise auf <a href="http://picocom.net" target="_blank">Picocom.net</a>.</p>
 <p>Das OCS verwendet <a href="http://netty.io/" target="_blank">Netty</a>, <a href="https://jquery.org/license/" target="_blank">jQuery</a> (plugins: <a href="https://github.com/SoapBox/jQuery-linkify/" target="_blank">linkify</a>, <a href="http://ned.im/noty/" target="_blank">noty</a>), Scramblegeneratoren von <a href="http://www.speedsolving.com/forum/showthread.php?25790-Prisma-Puzzle-Timer" target="_blank">Walter Souzas Prisma Puzzle Time</a> und icons von <a href="http://www.famfamfam.com/" target="_blank">famfamfam.com</a></p>
 
 
